2470a72784facf101e4d5b6314469ea8c6600a13
Liam Power <liamfpower@gmail.com>
2021-10-13 09:50:00 +1100

    started PEPESTREAM

--- PEPESTREAM/sketch/sketch.js
+++ PEPESTREAM/sketch/sketch.js
@@ -1,9 +1,11 @@
 let pepes = [];
-const numPepes = 50;
+const numPepes = 100;
 let p;
+p5.disableFriendlyErrors = true; // disables FES
+
 class PepeIcon {
-  constructor(x = null, y = null) {
-    this.icon = loadImage("PEPEFACE.png");
+  constructor(icon, x = null, y = null) {
+    this.icon = icon;
     this.x = x || Math.random() * 400 - 99;
     this.y = y || Math.random() * 560;
     this.w = 200;
@@ -11,61 +13,85 @@ class PepeIcon {
     this.accelX = Math.random() * 5 - 2.5;
     this.accelY = Math.random() * 5 - 2.5;
     this.color = (255, 255, 255, 127);
-    this.speed = 2 + Math.random() * 10;
+    this.speed = 2 + Math.random() * 5;
+    this.speedOffset = this.speed - this.speed / 2;
+  }
+
+  checkPos() {
+    if (this.x < 0 - this.w) this.x = width;
+    if (this.x > width + this.w) this.x = 0 - this.w;
+    if (this.y < 0 - this.h) this.y = height;
+    if (this.y > height + this.h) this.y = 0 - this.h;
   }
   draw() {
     push();
-    rectMode(CENTER);
     translate((this.x += this.accelX), (this.y += this.accelY));
-    this.rotation = (this.accelX + this.accelY) * 0.0001;
-    rotate((PI / 2) * this.rotation);
-    image(this.icon, 0, 0, this.w, this.h);
-    this.accelY = (mouseY / height) * this.speed - this.speed / 2;
-    this.accelX = (mouseX / width) * this.speed - this.speed / 2;
+    image(this.icon, 0, 0);
 
+    if (mouseX || mouseY) {
+      this.accelX = (mouseX / width) * this.speedOffset;
+      this.accelY = (mouseY / height) * this.speedOffset;
+    }
     pop();
-    if (this.x < 0 - this.w) {
-      // it's gone off the chain
-      this.x = width;
-    }
-    if (this.y < 0 - this.h) {
-      // it's gone off the chain
-      this.y = height;
-    }
-    if (this.y > height + this.h) {
-      // it's gone off the chain
-      this.y = 0 - this.h;
-    }
-    if (this.x > width + this.w) {
-      // it's gone off the chain
-      this.x = 0 - this.w;
-    }
+    if (frameCount % 30 == 0) this.checkPos();
   }
+}
+
+const timeFunction = (func) => {
+  console.time(`${func.name}`);
+  func();
+  console.timeEnd(`${func.name}`);
+};
+
+function drawFrameRate() {
+  let fps = frameRate();
+  fill(255);
+  stroke(0);
+  text("FPS: " + fps.toFixed(2), 10, 10);
 }
 
 let bg;
+let icon;
+let canvas;
+
 function preload() {
-  console.log("Hello");
+  icon = loadImage("PEPEFACE.png");
+
   for (let i = 0; i < numPepes; i++) {
-    pepes.push(new PepeIcon());
+    pepes.push(new PepeIcon(icon));
   }
-  console.log(pepes);
+  if (window.top != window.self) {
+    console.log("in iframe!");
+    document.querySelector("body").classList.add("in-iframe");
+    // In a Frame or IFrame
+  } else {
+    console.log("not in iframe!");
+  }
 }
 function setup() {
-  createCanvas(400, 560);
+  drawingContext.imageSmoothingEnabled = false;
+
+  canvas = createCanvas(400, 560, P2D);
+  pixelDensity(1);
+
   background(0, Math.random() * 127, 0);
   rectMode(CENTER);
 }
 
 function mousePressed() {
-  pepes.push(new PepeIcon(mouseX, mouseY));
+  pepes.shift();
+  pepes.push(new PepeIcon(icon, mouseX - 100, mouseY - 100));
 }
-function draw() {
-  push();
-  tint(100);
 
-  pop();
-  //   background(0, noise(deltaTime * 0.005) * 127, 0);
+function draw() {
+  // background(4, 78, 126);
+
+  if (frameCount % 10 == 0) {
+    push();
+
+    background(78, 126, 4, 15);
+    pop();
+  }
 
   if (pepes) {
     for (let pepe in pepes) {
@@ -76,9 +102,10 @@ function draw() {
         pepe.y < 0 - pepe.h ||
         pepe.y > height + pepe.h
       ) {
-        delete pepes[pepe];
-        pepes.push(new PepeIcon());
+        pepes.shift();
+        pepes.push(new PepeIcon(icon));
       }
     }
   }
+  drawFrameRate();
 }
